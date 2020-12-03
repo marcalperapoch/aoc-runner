e4d22bba1b370266ec903d713bd396fbf13b403c
marcal.perapoch <marcal.perapoch@king.com>
2020-12-03 19:50:23 +0100

    Added documentation for Custom types support

--- src/test/java/com/perapoch/aoc/runner/CustomParserSupportTest.java
+++ src/test/java/com/perapoch/aoc/runner/CustomParserSupportTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 public class CustomParserSupportTest {
 
